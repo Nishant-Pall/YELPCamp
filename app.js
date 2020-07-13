8d8cb0cd6dff4f0e9383b81c3c18dfa8317acd49
nishant <palln6935@gmail.com>
2020-07-13 17:39:48 +0530

    refactor code

--- app.js
+++ app.js
@@ -2,84 +2,61 @@ const express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser'),
 	mongoose = require("mongoose"),
-	PORT = 1000
+	PORT = 1000,
+	Campground = require("./models/campgrounds")
 
-mongoose.connect("mongodb://localhost/yelp_camp", {useUnifiedTopology:true, useNewUrlParser:true});
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine','ejs');
+mongoose.connect("mongodb://localhost/yelp_camp", { useUnifiedTopology: true, useNewUrlParser: true });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
-// SCHEMA SETUP
-const campgroundSchema = new mongoose.Schema({
-	name: String,
-	image: String,
-	description: String,
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema)
-
-// Campground.create(
-// 		{
-// 			name:"Granite Hill",
-// 			image:"https://images.pexels.com/photos/1840421/pexels-photo-1840421.jpeg?auto=compress&cs=tinysrgb&h=350",
-// 			description:"This is a huge granite hill, no bathroom, no water, Beautiful Granite!"
-// 		}, (err, campground) => {
-// 				if(err){
-// 					console.log(err);
-// 				} else{
-// 					console.log("Newly created campground");
-// 					console.log(campground);
-// 				}
-// 			}
-// );
-
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
 	res.render('landing');
 });
 // INDEX - show all campgrounds
-app.get('/campgrounds', (req, res)=>{
+app.get('/campgrounds', (req, res) => {
 	// Get all campgrounds from db
-	Campground.find({}, (err, allCampgrounds)=>{
-		if(err){
+	Campground.find({}, (err, allCampgrounds) => {
+		if (err) {
 			console.log(err);
-		} else{
-			res.render("index",{campgrounds:allCampgrounds});
+		} else {
+			res.render("index", { campgrounds: allCampgrounds });
 		}
 	})
 });
 // CREATE - add new campground to DB
-app.post('/campgrounds',(req, res)=>{
+app.post('/campgrounds', (req, res) => {
 	// get data from and make an object
 	var name = req.body.name;
 	var image = req.body.image;
 	var desc = req.body.description
-	var newCampground = {name:name, image:image, description:desc};
+	var newCampground = { name: name, image: image, description: desc };
 	// Create a new campground and save to DB
-	Campground.create(newCampground,(err, newlyCreated)=>{
-		if(err){
+	Campground.create(newCampground, (err, newlyCreated) => {
+		if (err) {
 			console.log(err);
-		} else{
-			res.redirect('/campgrounds');		
-		  }
-	})	
+		} else {
+			res.redirect('/campgrounds');
+		}
+	})
 });
 // NEW - show form to create campground
-app.get('/campgrounds/new',(req, res)=>{
+app.get('/campgrounds/new', (req, res) => {
 	res.render('new');
 });
 
 // SHOW - shows more info about one campground
-app.get('/campgrounds/:id',(req, res)=>{
+app.get('/campgrounds/:id', (req, res) => {
 	// find the campground with provided ID
-	Campground.findById(req.params.id, (err, foundCampground)=>{
-		if(err){
+	Campground.findById(req.params.id, (err, foundCampground) => {
+		if (err) {
 			console.log(err)
-		} else{
+		} else {
 			// render show template with that campground
-			res.render("show" ,{campground:foundCampground});
+			res.render("show", { campground: foundCampground });
 		}
 	})
 })
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
 	console.log("The YelpCamp Server Has Started!!")
 })
